eb1e6fd280a0a94adf3a79988a1fa62610188d03
Bibek2307 <bibek.mukherjee2307@gmail.com>
2025-08-11 17:00:44 +0530

    commit

---
+++ src/components/About.tsx
@@ -0,0 +1,28 @@
+
+
+const About = () => {
+    return (
+        <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
+                    About Bibek
+                </h2>
+                <p className="text-lg text-gray-700">
+                    Bibek Mukherjee is a passionate and versatile Computer Science student with a knack for blending creativity and technology.
+                    Skilled in languages like C++, Java, and PHP, along with web development technologies such as HTML, CSS, JavaScript, and React, Bibek has built a diverse portfolio.
+                    His expertise extends to backend development using Spring Framework and PostgreSQL, as well as AI/ML concepts for real-time analysis and prediction.
+                    This wide-ranging skill set has allowed him to craft innovative projects, whether it's enhancing user experience in the frontend or architecting efficient data-driven systems in the backend.
+                </p>
+                <p className="mt-4 text-lg text-gray-700">
+                    However, it was his love for User Interface Design that truly set him
+                    apart. Bibek's dedication to honing his skills in this area led him
+                    down a path of self-discovery, taking advantage of every resource
+                    available to him - from online tutorials to workshops and beyond. With
+                    his tireless efforts.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About
